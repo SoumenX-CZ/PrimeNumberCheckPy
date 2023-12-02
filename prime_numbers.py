4ba641f6d4469b37db2181e2cbac48b139fe530b
SoumenX-CZ <90548394+SoumenX-CZ@users.noreply.github.com>
2023-12-02 20:50:27 +0100

    Update prime_numbers.py
    Comments changed.

--- prime_numbers.py
+++ prime_numbers.py
@@ -3,12 +3,13 @@ num = int(input("Number: "))
 
 flag = False
 
+#Defining one. One is not a prime number. Any other numbers are in a "for" loop.
 if num == 1:
     print(num, "is not a prime number")
     
 #Checking if it is a prime number
 elif num > 1:
-    #Basic for loop
+    #Basic "for" loop in range from 2 to the input number
     for i in range(2, num):
         if (num % i) == 0:
             
